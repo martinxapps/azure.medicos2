59e38a9f39ce3492454c379552fc6a9d9a6175ce
Pablo Aguilar <paguilar@timer.ec>
2023-09-08 17:40:44 -0500

    links honorarios

--- src/views/IngresoMedicosView.vue
+++ src/views/IngresoMedicosView.vue
@@ -200,7 +200,7 @@ onMounted(async () => {
                            alt="microsift icon" width="30px" height="28px">
                       {{ language === "es" ? "Ingreso seguro" : "Log In" }}
                     </button>
-                    <p class="span-text mt-4 my-2">
+                    <p class="span-text my-2">
                       Ingrese con el usuario proporcionado por el hospital seguido de @hmetro.med.ec <br>
                       Su contraseña es la misma proporcionada por el hospital.<br>
                       Ej: Si su usario es pperez ustede deberá ingresar pperez@hmetro.med.ec
